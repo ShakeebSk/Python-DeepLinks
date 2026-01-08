1487c4c9b60d40e4aedf50dc34326b5ffb9a98b1
Kishan Dev <kishandev2509@gmail.com>
2026-01-08 19:22:12 +0530

    fixed formatting

--- tests/test_facebook.py
+++ tests/test_facebook.py
@@ -14,7 +14,7 @@ import pytest
 def test_facebook_deep_link_generation(url, expected_url):
     handler = FacebookHandler(url)
     match = handler.match_pattern()
-    expected_result ="facebook.com/" + expected_url
+    expected_result = "facebook.com/" + expected_url
 
     assert match is not None
     assert match.get("expected_url") == expected_url
@@ -23,7 +23,6 @@ def test_facebook_deep_link_generation(url, expected_url):
         deep_links = handler.generate_url(match)
     except AttributeError:
         pytest.fail("Implementation raised AttributeError, likely due to dict/object mismatch in generate_url")
-
 
     assert deep_links["web"] == url.replace("https://", "").replace("http://", "")
     assert deep_links["ios"] == f"fb://facewebmodal/f?href={expected_result}"
